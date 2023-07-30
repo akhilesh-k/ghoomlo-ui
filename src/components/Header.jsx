b98fb0d43957ed0549c0dda7c1d590f8fce6df70
Akhilesh K <akhilesh_k@outlook.com>
2023-07-31 05:00:18 +0530

    Mofifies the app logo imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,88 +1,71 @@
-import {
-  useRef,
-  useState
-} from "react"
-import { Link } from "react-router-dom"
-import "./css/header.css"
-import "./css/colors.css"
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import IconClose from "@/assets/icons/icon-close.svg";
+import HamBurger from "@/assets/icons/icon-hamburger-menu.svg";
+import AppLogo from "@/assets/images/app-logo.svg";
+import "./css/header.css";
+import "./css/colors.css";
 
 const HamburgerMenuComp = () => {
-  const [showMenu, setShowMenu] = useState(false)
-  
+  const [showMenu, setShowMenu] = useState(false);
+
   if (showMenu) {
     return (
       <div className="dropdown-container">
         <img
-          src="src/assets/icons/icon-close.svg"
+          src={IconClose}
           width="18px"
           height="18px"
           onClick={() => setShowMenu(false)}
         />
-        <div className="dropdown">
-        </div>
+        <div className="dropdown"></div>
       </div>
-    )
+    );
   } else {
     return (
       <img
-        src="src/assets/icons/icon-hamburger-menu.svg"
+        src={HamBurger}
         width="25.143px"
         height="20.354px"
         onClick={() => setShowMenu(true)}
       />
-    )
+    );
   }
-}
+};
 
 const CallPanel = ({ phoneNumber, makeCall }) => {
   return (
-    <div
-      className="panel"
-      onClick={makeCall}
-    >
-      <img
-        src="src/assets/icons/icon-call.svg"
-        width="15px"
-        height="16px"
-      />
-      <strong className="white">
-        {phoneNumber}
-      </strong>
+    <div className="panel" onClick={makeCall}>
+      <img src="src/assets/icons/icon-call.svg" width="15px" height="16px" />
+      <strong className="white">{phoneNumber}</strong>
     </div>
-  )
-}
+  );
+};
 
 const Header = () => {
-  const phone = useRef(6200944189)
+  const phone = useRef(6200944189);
 
   const makeCall = (e) => {
-    e.stopPropagation()
-    e.preventDefault()
-    window.location.href = `tel:${phone.current}`
-  }
+    e.stopPropagation();
+    e.preventDefault();
+    window.location.href = `tel:${phone.current}`;
+  };
 
   return (
     <nav className="primary-background">
       <ul>
         <li>
           <Link to="/">
-            <img
-              src="src/assets/images/app-logo.svg"
-              width="147.5px"
-              height="44px"
-            />
+            <img src={AppLogo} width="147.5px" height="44px" />
           </Link>
         </li>
         <li>
-          <CallPanel
-            phoneNumber={phone.current}
-            makeCall={makeCall}
-          />
+          <CallPanel phoneNumber={phone.current} makeCall={makeCall} />
         </li>
       </ul>
       {/* <HamburgerMenuComp /> */}
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
